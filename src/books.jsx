4b2531111d11e22b66b0d3cd0794b3fb014cfb40
Anshuman56 <anshumandehury@yandex.com>
2026-08-28 09:49:45 +0530

    add the delete handler

--- src/books.jsx
+++ src/books.jsx
@@ -60,6 +60,23 @@ export default function Books() {
     }
   }
 
+  async function handleDelete(id) {
+    setLoading(true);
+    setError("");
+    try {
+      const data = await apiFetch(`/books/${id}`, {
+        method: "delete",
+      });
+      console.log(data);
+      setResult(data);
+    } catch (err) {
+      console.log(err.message);
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
     <div className="max-w-2xl mx-auto p-4">
       <div className="flex justify-between items-center w-full mb-5">
@@ -117,7 +134,7 @@ export default function Books() {
                 </span>
                 <button
                   className="text-xs ml-2 cursor-pointer text-red-600 hover:text-red-800"
-                  onClick={() => handleDelet(item._id)}
+                  onClick={() => handleDelete(item._id)}
                 >
                   Delete
                 </button>
