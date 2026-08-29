25514e5650840a119f3ef011703a709ae0a6e073
Anshuman56 <anshumandehury@yandex.com>
2026-08-29 08:31:38 +0530

    fix: home page

--- src/home.jsx
+++ src/home.jsx
@@ -2,14 +2,16 @@ import { Link } from "react-router";
 
 export default function Home() {
   return (
-    <div className="">
-      <Link className="p-4 underline" to={"/login"}>
-        Login
-      </Link>
-      <Link className="p-4 underline" to={"/register"}>
-        Register
-      </Link>
-      <h1>
+    <div className="flex flex-col items-center justify-center mt-20 px-4 text-center">
+      <div>
+        <Link className="p-4 underline" to={"/login"}>
+          Login
+        </Link>
+        <Link className="p-4 underline" to={"/register"}>
+          Register
+        </Link>
+      </div>
+      <h1 className="text-4xl sm:text-5xl font-extrabold tracking-tight text-slate-900">
         Welcome to{" "}
         <Link className="underline" to={"/books"}>
           Books
